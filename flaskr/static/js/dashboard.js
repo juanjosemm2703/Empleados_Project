4ce495d9659246bb7fa23fd2f611bb131e188f19
Sebitasorange <vargassj@uninorte.edu.co>
2021-10-21 12:55:43 -0500

    Dashboard con archivos JS + SRC de chart JS en carpeta base.HTML

--- flaskr/static/js/dashboard.js
+++ flaskr/static/js/dashboard.js
@@ -1,23 +1,21 @@
-const MiGrafica1= document.getElementById("grafica1").getContext("2d")
-console.log(MiGrafica1)                   
-    var chart = new Chart(MiGrafica1,{
-        type:"bar",
-        data:{
-            labels:["Vino","Tequila","Cerveza","Ron","Aguardiente"],
-            datasets:[
-                {
-                    label:"Mi Grafica de Bebidas",
-                    background:"rgb(0,0,0)",
-                    borderColor:"rgb(255,255,0)",
-                    data:[12,39,5,30,55]
-                }
-            ]
-                            }
-                    
-                        })
+var MiGrafica1= document.getElementById("grafica1").getContext("2d")
+var chart = new Chart(MiGrafica1,{
+    type:"bar",
+    data:{
+        labels:["Vino","Tequila","Cerveza","Ron","Aguardiente"],
+        datasets:[
+            {
+                label:"Mi Grafica de Bebidas",
+                background:"rgb(0,0,0)",
+                borderColor:"rgb(255,255,0)",
+                data:[12,39,5,30,55]
+            }
+        ]
+    }
 
-const MiGrafica2 =document.getElementById("grafica2");
-console.log(MiGrafica2) 
+})
+
+var MiGrafica2 =document.getElementById("grafica2");
 var myPieChart = new Chart(MiGrafica2, {
     type: "pie",
     data: {
@@ -29,3 +27,38 @@ var myPieChart = new Chart(MiGrafica2, {
         }]
     },
 });
+
+var MiGrafica3= document.getElementById("grafica3").getContext("2d")                    
+var chart = new Chart(MiGrafica3,{
+    type:"line",
+    data:{
+        labels:["Vino","Tequila","Cerveza","Ron"],
+        datasets:[
+            {
+                label:"Mi Grafica de Bebidas",
+                background:"rgb(0,0,0)",
+                borderColor:"rgb(255,255,0)",
+                data:[12,39,5,30]
+            }
+        ]
+    }
+
+})
+
+
+var MiGrafica4= document.getElementById("grafica4").getContext("2d")
+var chart = new Chart(MiGrafica4,{
+    type:"bar",
+    data:{
+        labels:["Vino","Tequila","Cerveza","Ron","Aguardiente"],
+        datasets:[
+            {
+                label:"Mi Grafica de Bebidas",
+                background:"rgb(0,0,0)",
+                borderColor:"rgb(255,255,0)",
+                data:[12,39,5,30,55]
+            }
+        ]
+    }
+
+})
