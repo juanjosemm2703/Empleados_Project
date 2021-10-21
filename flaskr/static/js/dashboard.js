ae7dc11f3c01585eae02177dea2603ba3739d837
Sebitasorange <vargassj@uninorte.edu.co>
2021-10-21 14:16:20 -0500

    Dashboard

--- flaskr/static/js/dashboard.js
+++ flaskr/static/js/dashboard.js
@@ -58,7 +58,7 @@ var chart = new Chart(MiGrafica3,{
                 label:"Mi Grafica de Bebidas",
                 background:"rgb(0,0,0)",
                 borderColor:"rgb(255,255,0)",
-                data:[12,39,5,30]
+                data:[12,39,5,30,0]
             }
         ]
     }
@@ -70,15 +70,47 @@ var MiGrafica4= document.getElementById("grafica4").getContext("2d")
 var chart = new Chart(MiGrafica4,{
     type:"bar",
     data:{
-        labels:["Vino","Tequila","Cerveza","Ron","Aguardiente"],
+        labels:["Indefinido","Fijo","Prestación de servicios","Hora labor"],
         datasets:[
             {
-                label:"Mi Grafica de Bebidas",
-                background:"rgb(0,0,0)",
-                borderColor:"rgb(255,255,0)",
-                data:[12,39,5,30,55]
+                label:"Cantidad de contratos",
+                data: [100, 50, 40, 4, 0],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            indexAxis: 'y',
+            // Elements options apply to all of the options unless overridden in a dataset
+            // In this case, we are setting the border of each horizontal bar to be 2px wide
+            elements: {
+              bar: {
+                borderWidth: 2,
+              }
+            },
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'right',
+              },
+              title: {
+                display: true,
+                text: 'Chart.js Horizontal Bar Chart'
+              }
             }
-        ]
-    }
+          },
 
 })
