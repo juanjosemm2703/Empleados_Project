880249600b549ea885797d766e7079670278f3e1
Sebitasorange <vargassj@uninorte.edu.co>
2021-10-21 13:41:39 -0500

    Dashboard mejorado:
    Colocamos dependencias en la grafica1, fue necesario dejar un 0 para que la grafica comenzara desde 0 y no desde el numero mas bajo

--- flaskr/static/js/dashboard.js
+++ flaskr/static/js/dashboard.js
@@ -2,28 +2,48 @@ var MiGrafica1= document.getElementById("grafica1").getContext("2d")
 var chart = new Chart(MiGrafica1,{
     type:"bar",
     data:{
-        labels:["Vino","Tequila","Cerveza","Ron","Aguardiente"],
-        datasets:[
-            {
-                label:"Mi Grafica de Bebidas",
-                background:"rgb(0,0,0)",
-                borderColor:"rgb(255,255,0)",
-                data:[12,39,5,30,55]
+        labels:["Administrativo","Gerencia","Ingeniería","Producción","Sistemas"],
+        datasets: [{
+            label: 'Calificaciones',
+            data: [5, 3, 2, 4, 3, 0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
             }
-        ]
+        }
     }
 
 })
+
+
 
 var MiGrafica2 =document.getElementById("grafica2");
 var myPieChart = new Chart(MiGrafica2, {
     type: "pie",
     data: {
-        labels: ['año', 'mes', 'dia'],
+        labels: ["Administrativo","Gerencia","Ingeniería","Producción","Sistemas"],
         datasets: [{
             label:"Resultados",
-            data: [450,200,100],
-            backgroundColor: ["#204862", "#757439", "#1c835b"]
+            data: [90,10,40,85,20],
+            backgroundColor: ["#1B5B88", "#21AB19", "#B24519","#CBDF06", "#" ]
         }]
     },
 });
